0fcadb3700cd025a31d710c938e2e3c3ac201ba3
remote-torrent-6747 <abbyc@sky.com>
2021-04-20 21:52:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
     erb :recipe
   end
 
-  delete '/articles/:id' do
+  delete '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
     redirect "/recipes"
